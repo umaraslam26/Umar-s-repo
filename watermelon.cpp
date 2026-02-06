035c98714e76c040310b01d3fb4e028bfd12b0e7
Umar <aslamumar998@gmail.com>
2026-02-06 21:13:25 +0100

    Adding watermelon.cpp to github

---
+++ watermelon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cout << "";
+    cin >> n;
+    if (n%2==0){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
